f83ef6b31a64294e3a89ad13d90d258bbecb7cbb
Vikas Landge <vikasslandge@gmail.com>
2019-04-28 19:04:07 +0530

    insert and show list of bus and provider

---
+++ BusProviderApplication/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusProviderApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProviderServiceReference.ProvidersWebServiceSoapClient soapClient = new ProviderServiceReference.ProvidersWebServiceSoapClient();
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public PartialViewResult GetProviders()
+        {
+            var result = soapClient.GetProviderDetails();
+            
+            return PartialView("_ProviderList",result);
+        }
+
+        public PartialViewResult InsertProviderDetail()
+        {
+            //var result = soapClient.InsertIntoProvider();
+
+            return PartialView("_InsertProvider");
+        }
+        [HttpPost]
+        public ActionResult InsertProviderDetail(FormCollection formCollection)
+        {
+            soapClient.InsertIntoProvider(formCollection["OrganisationName"],formCollection["ContactNo"]);
+
+            return RedirectToAction("GetProviders");
+        }
+        public PartialViewResult InsertBusDetail()
+        {
+             
+            ViewBag.ProviderList = new SelectList(soapClient.GetProviderDetails(), "ProviderID", "OrganisationName");
+
+            return PartialView("_AddBus");
+        }
+        [HttpPost]
+        public ActionResult InsertBusDetail(FormCollection formCollection)
+        {
+            soapClient.InsertBusDetails(formCollection["BusName"],Convert.ToInt32( formCollection["Capacity"]), formCollection["Type"], formCollection["BusNo"],Convert.ToInt32( formCollection["ProviderList"]));
+
+            return RedirectToAction("GetBusDetails");
+        }
+        public PartialViewResult GetBusDetails()
+        {
+             
+
+            return PartialView("_GetBusDetails",soapClient.GetBusDetails());
+        }
+    }
+}
